7122db01d255ae458db055d8209cf91ceed40e04
tembot-umich <humpheryjaami@gmail.com>
2023-06-15 12:17:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/*
+ *main-runs all the code
+ *
+ *description: Runs puts and print statement
+ *return; Return (0) to exit function
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
