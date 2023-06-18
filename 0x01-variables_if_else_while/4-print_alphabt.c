36c5e45e0029ffd9f1bdafe6dc00bd153ff6cca2
tembot-umich <humpheryjaami@gmail.com>
2023-06-17 22:13:56 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (i != 'e' && i i != 'q')
+		{
+			putcha (i);
+		}
+	putchar('\n');
+	return (0)
+}
