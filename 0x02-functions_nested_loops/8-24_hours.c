4a8a76ed3c60b511e3915c7f5c51840e57fa27be
tembot-umich <humpheryjaami@gmail.com>
2023-06-21 13:50:46 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer -prints every minute of the day of him
+ *
+ * Return: ...
+ */
+void jack_bauer(void)
+{
+	int m,n,b,v;
+
+	for (m=0; m <= 2; m++)
+	{
+		for (n = 0; n <= 9; n++)
+		{
+			if ((m <= 1 && n <=9 || (m <= 2 && n <=3))
+			{
+				for (b = 0; b <= 5; b++)
+				{
+					for (v = 0; v <= 9; d++)
+					{
+				        	_putchar(m + '0';
+						_putchar(n + '0';
+						_putchar(58);
+						_putchar(b + '0');
+						_putchar(v + '0');
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
+}
+
+
