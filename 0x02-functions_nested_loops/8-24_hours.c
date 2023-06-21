36ce632fb4690ae2bcb0f76b67d73f52e0ca841c
tembot-umich <humpheryjaami@gmail.com>
2023-06-21 14:36:13 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,17 +18,27 @@ void jack_bauer(void)
 				{
 					for (c = 0; c <=9; c++)
 					{
-						  if ((a <= 1 && z <= 9) || (a <= 2 && z <= 3))
-                        {
-                                for (x = 0; x <= 5; x++)
-                                {
-                                        for (c = 0; c <= 9; c++)
-					{
-                                                _putchar(a + '0');
-                                                _putchar(z + '0');
-                                                _putchar(58);
-                                                _putchar(x + '0');
-                                                _putchar(c + '0');
-                                                _putchar('\n');
+						if ((a <= 1 && z <= 9) || (a <= 2 && z <= 3))
+                        			{
+                               				 for (x = 0; x <= 5; x++)
+                               				 {
+                               			        	 for (c = 0; c <= 9; c++)
+					                         {
+                                        		  		_putchar(a + '0');
+                                        	          		_putchar(z + '0');
+                                        	          		_putchar(58);
+                                               		  		_putchar(x + '0');
+                                              		  		_putchar(c + '0');
+                                                          		_putchar('\n');
+								}
+							}
+						}
+					}
+
+				}
+			}
+		}
+	}
+}
 
 
