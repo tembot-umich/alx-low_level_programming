a630372966dc04ecc4d05a0ef928c089f4b0c27e
tembot-umich <humpheryjaami@gmail.com>
2023-06-22 07:01:56 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _supper - uppercase letters.
+ * @c: character to be checked
+ *
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+
+{
+	if (c >= 'A' && c <= 'X')
+		return (1);
+	else
+		return (0);
+
+}
