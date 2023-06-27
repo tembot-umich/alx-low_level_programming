2ca1b8d91153232f4ece6c106078b67c22180d58
tembot-umich <humpheryjaami@gmail.com>
2023-06-27 14:50:31 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,25 +6,23 @@ Return: print
 */
 void puts2(char *str)
 {
-	int length = 0;
+	int longi = 0;
 	int t = 0;
-	char *y =str;
-	int i;
+	char *y = str;
+	int o;
 
-	while (*y =str;
+	while (*y != '\0')
 	{
-		y++'
-		length ++
+		y++;
+		longi++;
 	}
-	t = length - 1;
-	for (i = 0; i <= t; 1++)
+	t = longi - 1;
+	for (o = 0; o <= t; o++)
 	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		]
-		_putchar('\n')
-
-
-
-
+		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
+	}
+	}
+	_putchar('\n');
+}
