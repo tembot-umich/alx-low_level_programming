a1f64feb343ce564db61545480d63dbf9e561815
tembot-umich <humpheryjaami@gmail.com>
2023-06-29 15:11:33 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string params
+ *
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	int t;
+	int y;
+	char data1[] =
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] =
+"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (t = 0; s[t] != '\0'; t++)
+	{
+		for (y = 0; y < 52; y++)
+		{
+			if (s[t] == data1[y])
+			{
+				s[t] = datarot[y];
+				
+				break;
+			}
+		}
+	}
+	return (s);
+}
+
