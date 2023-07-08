3fbf3e157cb1db55912093f262df0da74c54fa85
tembot-umich <humpheryjaami@gmail.com>
2023-07-08 13:56:20 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - writes name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (success)
+ */
+int main(int argc_attribute_((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+arg v tasks
