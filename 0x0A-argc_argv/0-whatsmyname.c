e3edcdb8f6d403d941deb3a0acede644b843b746
tembot-umich <humpheryjaami@gmail.com>
2023-07-09 09:52:48 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,9 @@
  * Return: always 0 (success)
  */
 
-int main(int argc__attribute__((unused)), char *argv[])
+int main(int argc, char **argv)
 {
-	printf("%s\n", *argv);
-
-	return (0);
+        (void)argc;
+        printf("%s\n", argv[0]);
+        return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,9 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int_putchar(char c);
-int_atoi(char *s);
+int _putchar(char c);
+int _atoi(char *s);
 
 #endif
-~                                                                                           
-~          
