b2180df43a76660469595c9eadd79232e3d48460
tembot-umich <humpheryjaami@gmail.com>
2023-08-03 12:54:24 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - outputs the binary equivalent of a decimal number
+ * @n: number to output in binary
+ */
+void print_binary(unsigned long int n)
+{
+	int k, num = 0;
+	unsigned long int future;
+
+	for (k = 33; k >= 0; k--)
+	{
+		future = n >> k;
+
+		if (future & 1)
+		{
+			_putchar('1');
+			num++;
+		}
+		else if (num)
+			_putchar('0');
+	}
+	if (!num)
+		_putchar('0');
+}
+
